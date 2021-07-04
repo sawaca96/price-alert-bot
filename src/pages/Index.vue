cdb72a7c5cd3511fbd343902182561092cb649b0
sawaca96 <sawaca96@hanmail.net>
2021-07-04 20:21:38 +0900

    Conneect binance websocket

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,5 +1,21 @@
 <template>
   <q-page class="q-mt-sm">
+    <div class="q-pa-md q-gutter-sm">
+      <q-btn
+        :ripple="{ color: 'yellow' }"
+        color="secondary"
+        label="Subscribe"
+        no-caps
+        @click="subscribe"
+      />
+      <q-btn
+        :ripple="{ color: 'yellow' }"
+        color="secondary"
+        label="Unsubscribe"
+        no-caps
+        @click="unsubscribe"
+      />
+    </div>
     <List />
   </q-page>
 </template>
@@ -12,7 +28,34 @@ export default defineComponent({
   name: 'PageIndex',
   components: { List },
   setup() {
-    return {};
+    const ws = new WebSocket('wss://stream.binance.com:9443/ws');
+
+    // When message received from web socket then...
+    ws.onmessage = function (event) {
+      // Easier and shorter.
+      const data: Object = JSON.parse(event.data);
+      console.log(data);
+    };
+
+    const subscribe = () => {
+      const msg = {
+        method: 'SUBSCRIBE',
+        params: ['btcusdt@aggTrade', 'btcusdt@depth'],
+        id: 1,
+      };
+      ws.send(JSON.stringify(msg));
+    };
+
+    const unsubscribe = () => {
+      const msg = {
+        method: 'UNSUBSCRIBE',
+        params: ['btcusdt@aggTrade', 'btcusdt@depth'],
+        id: 1,
+      };
+      ws.send(JSON.stringify(msg));
+    };
+
+    return { ws, subscribe, unsubscribe };
   },
 });
 </script>
